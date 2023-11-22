47c6f5f181d560c369d269bf91cfe18c392ea6de
Joon2000 <najoonwon22@gmail.com>
2023-11-23 02:52:36 +0900

    Add Player props

--- frontend/components/Player.tsx
+++ frontend/components/Player.tsx
@@ -1,17 +1,41 @@
 import React, { useState } from "react"
+import { Coin } from "./Coin"
+import { EnteranceFee } from "./EnteranceFee"
+import { Cards } from "./Cards"
+import { Choice } from "./Choice"
 
-const Player = () => {
+const Player = ({
+  turn,
+  setTurn,
+  dealerCoin,
+  coin,
+  setCoin,
+  addCard,
+  setAddCard,
+  choice,
+  setChoice,
+}) => {
   const [payEnteranceFee, setPayEnteranceFee] = useState<boolean>(false)
-  const [coin, setCoin] = useState(0)
+  const [cards, setCards] = useState<string[]>(["", "", ""])
   return (
     <div>
-      <Coin />
+      <Coin coin={coin} />
       {!payEnteranceFee ? (
-        <EnteranceFee />
+        <EnteranceFee setPayEnteranceFee={setPayEnteranceFee} />
       ) : (
         <div>
-          <Cards />
-          <Choice />
+          <Cards cards={cards} addCard={addCard} />
+          <Choice
+            turn={turn}
+            setTurn={setTurn}
+            dealerCoin={dealerCoin}
+            coin={coin}
+            choice={choice}
+            setCoin={setCoin}
+            setCards={setCards}
+            setChoice={setChoice}
+            setAddCard={setAddCard}
+          />
         </div>
       )}
     </div>
