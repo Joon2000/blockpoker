2d320990546cc34016843b412d5d436995c4ed93
Joon2000 <najoonwon22@gmail.com>
2023-11-23 19:43:24 +0900

    Fix addCard result

--- frontend/components/Cards.tsx
+++ frontend/components/Cards.tsx
@@ -1,18 +1,18 @@
 import React from "react"
 
-const Cards = ({ cards, addCard }) => {
+const Cards = ({ playerCards, addCard }) => {
   return (
     <div>
       {addCard ? (
         <div>
-          <div>{cards[0]}</div>
-          <div>{cards[1]}</div>
+          <div>{playerCards[0]}</div>
+          <div>{playerCards[1]}</div>
+          <div>{playerCards[2]}</div>
         </div>
       ) : (
         <div>
-          <div>{cards[0]}</div>
-          <div>{cards[1]}</div>
-          <div>{cards[2]}</div>
+          <div>{playerCards[0]}</div>
+          <div>{playerCards[1]}</div>
         </div>
       )}
     </div>
