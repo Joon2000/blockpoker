d3a220544644753d813d8bc9e403e608fd6394b0
Joon2000 <najoonwon22@gmail.com>
2023-11-23 13:06:22 +0900

    Add round callstate

--- frontend/Type.ts
+++ frontend/Type.ts
@@ -1,3 +1,7 @@
 type Choice = "FOLD" | "CHECK" | "RAISE" | "NONE"
 
 type Turn = "DEALER" | "PLAYER" | "ENTERING" | "STARTING"
+
+type Round = "ROUND1" | "ROUND2"
+
+type CallState = "CALL" | "NOTCALL"
