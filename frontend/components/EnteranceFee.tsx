49c7ad576da5188f0547a506f314786db13f39ed
Joon2000 <najoonwon22@gmail.com>
2023-11-24 21:36:37 +0900

    Add setPlayerTotalBet

--- frontend/components/EnteranceFee.tsx
+++ frontend/components/EnteranceFee.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 
-const EnteranceFee = ({ setTurn, setCoin, setPlayerBet }) => {
+const EnteranceFee = ({
+  setTurn,
+  setCoin,
+  setPlayerBet,
+  setPlayerTotalBet,
+}) => {
   function sendFee(e: { preventDefault: () => void }) {
     e.preventDefault()
     console.log("fee send")
     setCoin((prevCoin: number): number => prevCoin - 1)
     setPlayerBet(1)
+    setPlayerTotalBet(1)
     //function seding fee to canister
     setTurn("STARTING")
   }
