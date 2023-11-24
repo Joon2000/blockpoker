956c78014f1e29d71a09b61b898773e4339507d2
Joon2000 <najoonwon22@gmail.com>
2023-11-24 21:36:20 +0900

    Delete choice is none case

--- frontend/temp/getDealerChoice.ts
+++ frontend/temp/getDealerChoice.ts
@@ -2,13 +2,12 @@ import { getRandomNumber } from "./randomNumber"
 
 export function getDealerChoice(choice: Choice): Choice {
   const randomNumber = getRandomNumber()
-  if (choice === "NONE") {
-    if (randomNumber <= 5) {
-      return "CALL"
-    } else if (randomNumber <= 8) {
-      return "RAISE"
-    } else {
-      return "FOLD"
-    }
+
+  if (randomNumber <= 5) {
+    return "CALL"
+  } else if (randomNumber <= 8) {
+    return "RAISE"
+  } else {
+    return "FOLD"
   }
 }
