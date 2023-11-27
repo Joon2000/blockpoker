44678b6b016e3e076786d9ce96b3c517bb71b426
Joon2000 <najoonwon22@gmail.com>
2023-11-27 10:59:18 +0900

    Delete Counter canister

--- src/declarations/counter/counter.did.js
+++
@@ -1,6 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'getRandomNumber' : IDL.Func([], [IDL.Opt(IDL.Nat8)], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
