9017dab0c3761791e9d5ec6c4a2903d2776b691a
Joon2000 <najoonwon22@gmail.com>
2023-11-30 02:19:31 +0900

    Add isReady

--- frontend/components/PlayerPage.tsx
+++ frontend/components/PlayerPage.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { PokerTable } from "./PokerTable"
 import { PlayerButton } from "./PlayerButton"
 import { Box } from "@mui/material"
 import { brown } from "@mui/material/colors"
 
 const PlayerPage = ({ wallet }) => {
+  const [isReady, setIsReady] = useState<Boolean>(false)
   return (
     <div style={{ marginTop: "40px" }}>
       <Box
