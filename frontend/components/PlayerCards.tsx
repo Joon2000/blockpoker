e43a5614f0b1c8d93dd283b4d7bb0daff5478ad2
Joon2000 <najoonwon22@gmail.com>
2023-12-01 04:07:11 +0900

    Add NumberCard

--- frontend/components/PlayerCards.tsx
+++ frontend/components/PlayerCards.tsx
@@ -1,20 +1,22 @@
 import { Grid } from "@mui/material"
 import React from "react"
 import { CardBack } from "./CardBack"
+import { NumberCard } from "./NumberCard"
 
-const PLayerCards = () => {
+const PlayerCards = ({ playerCards }) => {
   return (
-    <div style={{ position: "absolute", left: "37%", top: "380px" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={8}>
-          <CardBack />
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <CardBack />
-        </Grid>
+    <div style={{ position: "absolute", left: "32%", top: "380px" }}>
+      <Grid container spacing={4}>
+        {playerCards.map((card: number, index: React.Key) => {
+          return (
+            <Grid item xs={6} md={4} key={index}>
+              {card ? <NumberCard number={card} /> : <CardBack />}
+            </Grid>
+          )
+        })}
       </Grid>
     </div>
   )
 }
 
-export { PLayerCards }
+export { PlayerCards }
