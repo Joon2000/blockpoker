06acd976a740d1a5da816cc5eb067f0eb2a284ee
ptw <ptw@mirny.io>
2023-12-11 22:00:06 +0900

    connect with readyGame, startGame, endGame from backend

--- frontend/components/PlayerPageRefactor.tsx
+++ frontend/components/PlayerPageRefactor.tsx
@@ -13,6 +13,8 @@ import { poker} from "../../src/declarations/poker"
 import { Typography } from '@mui/material';
 import { SharedGameStatus, PlayingStatus, SharedPlayer } from "src/declarations/poker/poker.did"
 import { Principal } from "@dfinity/principal"
+import {QueueScreen} from "./QueueScreen"
+// import { Principal } from "@dfinity/candid/lib/cjs/idl"
 
 
 
@@ -25,6 +27,8 @@ const PlayerPageRefactor = ({ wallet }) => {
   const [isAllPlayerCall, setIsAllPlayerCall] = useState<boolean>(false);
   const [playerInfoArray, setPlayerInfoArray] = useState<SharedPlayer[]>([]);
 
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+
   useInterval(async () => {
     const gameStatus = await poker.getGameStatus();
     setPlayingStatus(Object.keys(gameStatus.playingStatus)[0]);
@@ -34,9 +38,44 @@ const PlayerPageRefactor = ({ wallet }) => {
 
     const playerInfoArray = await poker.getPlayerInfoArray();
     setPlayerInfoArray(playerInfoArray);
-    console.log("player info array :", playerInfoArray);
+    console.log("wallet :", wallet.principal);
 
   }, 2000)
+
+  async function enterGame(e: { preventDefault: any }) {
+    setIsButtonDisabled(true);
+    await poker.enterGame(Principal.fromText(wallet.principal));
+    setIsButtonDisabled(false);
+
+    e.preventDefault
+
+    console.log("Game enterGame")
+  };
+
+  async function readyGame(e: { preventDefault: any }) {
+    setIsButtonDisabled(true);
+    await poker.readyGame(Principal.fromText(wallet.principal));
+    setIsButtonDisabled(false);
+
+    e.preventDefault
+
+    console.log("Game readyGame")
+  };
+
+  async function startGame(e: { preventDefault: any }) {
+    setIsButtonDisabled(true);
+    await poker.startGame(Principal.fromText(wallet.principal));
+    setIsButtonDisabled(false);
+    console.log("Game startGame")
+  };
+
+  async function endGame(e: { preventDefault: any }) {
+    setIsButtonDisabled(true);
+    await poker.endGame();
+    setIsButtonDisabled(false);
+    console.log("Game endGame")
+  };
+
 
   return (
     <div style={{ marginTop: "40px" }}>
@@ -48,25 +87,57 @@ const PlayerPageRefactor = ({ wallet }) => {
           bgcolor: brown[100],
         }}
       >
+        <Typography>wallet principal : {wallet.principal}</Typography>
         <Typography>playingStatus : {playingStatus}</Typography>
         <Typography>masterPlayer : {masterPlayer}</Typography>
         <Typography>gameTurn : {gameTurn}</Typography>
         <Typography>isAllPlayerCall : {isAllPlayerCall.toString()}</Typography>
 
-        {playerInfoArray.map((playerInfo)=>(playerInfo.address.toString()))}
-
-        <PokerTableRefactor
-          wallet={wallet}
-        />
+        <Box>
+          <Typography>{playerInfoArray.map((playerInfo)=>(playerInfo.address.toString()))}</Typography>
+        </Box>
+        
+        {playingStatus == "PLAYING" 
+        ? <PokerTableRefactor wallet={wallet}/> : playingStatus == "GAME_END" 
+        ? <PokerTableRefactor wallet={wallet}/> : <QueueScreen wallet={wallet}/>}
+        
       </Box>
       
       <Button
         variant="contained"
-        // onClick={clickInitializeGame}
+        onClick={startGame}
         size="large"
         color="error"
+        disabled={isButtonDisabled}
       >
-        Initialize Game 123123
+        Start Game
+      </Button>
+      <Button
+        variant="contained"
+        onClick={readyGame}
+        size="large"
+        color="warning"
+        disabled={isButtonDisabled}
+      >
+        Ready Game
+      </Button>
+      <Button
+        variant="contained"
+        onClick={enterGame}
+        size="large"
+        color="primary"
+        disabled={isButtonDisabled}
+      >
+        Enter Game
+      </Button>
+      <Button
+        variant="contained"
+        onClick={endGame}
+        size="large"
+        color="primary"
+        disabled={isButtonDisabled}
+      >
+        End Game
       </Button>
     </div>
   )
