d44413d17455a520a8759df20e1bb45518d83691
ptw <ptw@mirny.io>
2023-12-17 23:22:34 +0900

    upgrad css style and add winner pop up

--- frontend/components/PlayerCards.tsx
+++ frontend/components/PlayerCards.tsx
@@ -2,24 +2,31 @@ import { Grid, Box, Typography } from "@mui/material"
 import React, { useEffect } from "react"
 import { TrumpCard } from "./TrumpCard"
 import { Card } from "src/declarations/poker/poker.did"
-import { blue, brown, pink } from "@mui/material/colors"
+import { blue, brown, grey, pink } from "@mui/material/colors"
 
 const PlayerCards = ({
   player,
+  wallet,
   gameTurn,
   currentPlayerCrpytoNumber,
 }) => {
 
   return (
     <Box 
-    sx={{bgcolor: player!=null && player.playerOrder.toString() == gameTurn.toString() ? blue[200] : pink[200]}}
+    sx={{
+      bgcolor: player!=null && player.address.toString() == wallet.principal.toString() ? pink[200] : blue[100],
+      border: player!=null && player.playerOrder.toString() == gameTurn.toString() ? 3 : 0,
+    }}
     >
+      {player!=null &&<Typography variant="caption">{player.address.toString()}</Typography>}
+      {player!=null &&<Typography>{Object.keys(player.bettingAction)[0].toString()}</Typography>}
+      {player!=null &&<Typography>currentChips : {player.currentChips.toString()}</Typography>}
+      {player!=null &&<Typography>currentBetAmount : {player.currentBetAmount.toString()}</Typography>}
       <Grid container spacing={0}>
-        {player!=null &&<Typography>{player.address.toString()}</Typography>}
-        {player!=null &&<Typography>{Object.keys(player.bettingAction)[0].toString()}</Typography>}
+        
         {player!=null &&
         player.cards.map((card : Card, index: React.Key) => (
-            <Grid item xs={3} key={index}>
+            <Grid item xs={2} key={index}>
               <TrumpCard 
               cardNumber={ Number(card.cardNumber)}
               order={card.order}
--- frontend/components/StateBox.tsx
+++ frontend/components/StateBox.tsx
@@ -8,19 +8,19 @@ const StateBox = ({
   return (
     <Box
       sx={{
-        width: 200,
-        height: 100,
-        borderRadius: 1,
-        bgcolor: grey[400],
+        // width: 200,
+        // height: 100,
+        // borderRadius: 1,
+        // bgcolor: grey[400],
       }}
     >
-      <Typography> Player Status</Typography>
+      {/* <Typography> Player Status</Typography>
       <Typography>playerOrder : {playerInfo.playerOrder.toString()}</Typography>
       <Typography>currentChips : {playerInfo.currentChips.toString()}</Typography>
       <Typography>currentBetAmount : {playerInfo.currentBetAmount.toString()}</Typography>
       <Typography>totalBetAmount : {playerInfo.totalBetAmount.toString()}</Typography>
       <Typography>playingState : {Object.keys(playerInfo.playingState)[0]}</Typography>
-      <Typography>bettingAction : {Object.keys(playerInfo.bettingAction)[0]}</Typography>
+      <Typography>bettingAction : {Object.keys(playerInfo.bettingAction)[0]}</Typography> */}
     </Box>
   )
 }
